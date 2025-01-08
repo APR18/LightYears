5a63e9a082e68820840800cddc7d583248267936
Anandhapadmanabhan R <anandhu.6231@gmail.com>
2025-01-08 19:35:08 +0000

    added Entrpoint , Lightyears Engine and LightYears Game projects

--- LightYears/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include<memory>
-#include<framework/Application.h>
-#include<SFML/Graphics.hpp>
-int main()
-{
-	std::unique_ptr<ly::Application> app = std::make_unique<ly::Application>();
-	app->run();
-}
